0182e3242252154b4aa332f307795f121c65a3ca
Pushcod <4programart1@gmail.com>
2024-03-27 19:33:53 +0300

    Создали отдельные страницы для Articles

--- src/app/(pages)/articles/[id]/page.tsx
+++ src/app/(pages)/articles/[id]/page.tsx
@@ -1,16 +1,28 @@
 "use client";
+import useSingleArticle from '@/Hooks/useSingleArticle';
+import Image from 'next/image';
 import React, { useEffect } from 'react'
 
 export default function page({params}) {
     
-    useEffect(()=>{
-        console.log(params.id);
-    },[]);
+    const detailArticle = useSingleArticle(params.id);
   
     return (
-    <div>
-      
-    </div>
+    <section className='w-full py-16'>
+      <div className="container mx-auto">
+        {/* Начало превью*/}
+        <div className="w-full h-[320px] md:h-[520px] overflow-hidden rounded-xl">
+        <img src={process.env.NEXT_PUBLIC_STRAPI_BACKEND_URL + detailArticle?.attributes?.image?.data.attributes?.url} alt="" />
+        </div>
+        {/* Конец превью*/}
+        {/* Начало названия*/}
+        <h2 className="text-left text-white uppercase text-5xl">{detailArticle?.attributes?.name}</h2>
+        {/* Конец названия*/}
+        {/* Начало описания*/}
+        <p className="text-zinc-700 text-base md:text-xl text-left mt-7">L{detailArticle?.attributes?.longText}</p>
+        {/* Конец описания*/}
+      </div>
+    </section>
   )
 }
 
--- src/components/utils/GlobalApi.tsx
+++ src/components/utils/GlobalApi.tsx
@@ -9,10 +9,15 @@ const axiosClient = axios.create({
     }
 });
 
+//API - запросы
 const getArticles = () => axiosClient.get('/articles?populate=*');
+//API - запрос на получение статьи
 const getBenefists = () => axiosClient.get('/preimushhestvas?populate=*');
+//API - запрос на получение конкретной статьи
+const getSingleArticle = (id) => axiosClient.get('/articles/' + id + '?populate=*');
 
 export default {
     getBenefists,
     getArticles,
+    getSingleArticle,
 }
